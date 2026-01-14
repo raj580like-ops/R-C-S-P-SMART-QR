f92a187f46e05f9487948b6ac2224f2c17dced0d
raj580like-ops <raj580like@gmail.com>
2026-01-14 22:02:54 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,14 +6,14 @@ import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager
 // 1. FIREBASE INITIALIZATION
 // ==========================================
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCABFo3Whsb3kFbfCiLU4jH4TPJjc-_3Yk",
+  authDomain: "r-c-s-p-qr.firebaseapp.com",
+  databaseURL: "https://r-c-s-p-qr-default-rtdb.firebaseio.com",
+  projectId: "r-c-s-p-qr",
+  storageBucket: "r-c-s-p-qr.firebasestorage.app",
+  messagingSenderId: "851473499622",
+  appId: "1:851473499622:web:be694ec06ba10d21148227"
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
