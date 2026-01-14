420b0fa0758ed1de93a8d7b3b4fd601ce5c088e6
raj580like-ops <raj580like@gmail.com>
2026-01-14 21:56:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,49 @@
-const CACHE_NAME = 'rcsp-v1';
-const ASSETS = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/js/firebase-config.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
-    'https://unpkg.com/html5-qrcode'
+const CACHE_NAME = 'rcsp-pos-v1';
+
+// Files to cache for offline use
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/js/app.js',
+  '/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+  'https://unpkg.com/html5-qrcode',
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700;800&display=swap'
 ];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then(res => res || fetch(e.request))
-    );
+// Network first, falling back to cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+// Activate & Cleanup old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
