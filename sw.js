b0bc3907462e64c0af91132441e2579336cc17f1
raj580like-ops <raj580like@gmail.com>
2026-01-14 19:24:04 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'rcsp-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/js/firebase-config.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+    'https://unpkg.com/html5-qrcode'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
+});
